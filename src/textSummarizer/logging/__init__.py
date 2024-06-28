7308977c8b61b9de4a88719f06965c4ad95374e9
Apoorv <apoorvsinghnegi18@gmail.com>
2024-06-28 16:30:51 +0530

    requirements and installation done

---
+++ src/textSummarizer/logging/__init__.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import logging
+
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
+#asctime = the time at which we run our code
+#levelname = what type of logging we are doing
+#modulename = the name of the module we are logging
+#message = message we are logging
+log_dir = "logs"
+log_filepath = os.path.join(log_dir,"running_logs.log")
+os.makedirs(log_dir, exist_ok=True)
+#exist_ok = True it means even if the directory exists, it will no raise any exception.
+
+
+
+logging.basicConfig(
+    level= logging.INFO,
+    format= logging_str,
+
+    handlers=[
+        logging.FileHandler(log_filepath),#to show logging info in file
+        logging.StreamHandler(sys.stdout)#to show logging info in terminal
+    ]
+)
+
+logger = logging.getLogger("textSummarizerLogger")
--- template.py
+++ template.py
@@ -53,4 +53,4 @@ for filepath in list_of_files:
 
     
     else:
-        logging.info(f"{filename} is already exists")
+        logging.info(f"{filename} is already exists") #if there is code in the file or it is not empty it will create a new file
